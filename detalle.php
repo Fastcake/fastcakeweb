7f4ec6f60ed625f800b82c6cdfa58646f1aeeec0
steelfun <alex_berrios_e@hotmail.com>
2014-05-20 16:44:28 -0400

    Archivos no necesarios, comienzo detalle producto
    Archivos no necesarios, comienzo detalle producto

--- detalle.php
+++ detalle.php
@@ -2,13 +2,14 @@
         include("header.php");
         include("coneccion.php");
 ?>
-
-        <section id="contenedor">
+<section id="contenedor">
             Listado de productos
             <section class="categoria">
                 
             <?php    
-                $registros=mysql_query("select id_producto, nombre_producto, descripcion_producto, precio_producto, tiempo_elaboracion_producto from producto order by precio_producto asc",$conexion) or
+
+            		$id=$_GET['id'];
+                $registros=mysql_query("select id_producto, nombre_producto, descripcion_producto, precio_producto, tiempo_elaboracion_producto from producto where id_producto = $id",$conexion) or
                 die("Problemas en el select:".mysql_error());
 
                 while ($reg=mysql_fetch_array($registros))
@@ -35,23 +36,3 @@
             </section>
 
         </section>
-       
-
-
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
-    </body>
-</html>
